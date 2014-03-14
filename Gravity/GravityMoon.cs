50a68ce6cb02c48236a287149dc58f14a4a84037
rslid <takepack@gmail.com>
2014-03-14 19:12:35 +0200

    homework

---
+++ Gravity/GravityMoon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gravity
+{
+    class GravityMoon
+    {
+        static void Main(string[] args)
+        {
+            //The gravitational field of the Moon is approximately 17% of that on the Earth. Write a program that calculates 
+            //the weight of a man on the moon by a given weight on the Earth. 
+
+            Console.WriteLine("This program calculates how much will be your weight on the Moon");
+            Console.Write("Please enter your weight on Earth in kg (81,2) : ");
+            double weight, moonWeight;
+            weight = Double.Parse(Console.ReadLine());
+
+            moonWeight = weight * 0.17;
+            Console.WriteLine("Your weight on the moon will be: {0}", moonWeight);
+
+        }
+    }
+}
---
+++ Odd or Even Integers/Odd_or_Even_Integers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_or_Even_Integers
+{
+    class Odd_or_Even_Integers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number for verification, result will be False for Even and True for Odd number: ");
+            int num;
+            num = Int32.Parse(Console.ReadLine());
+            Math.Abs(num);
+            if (Math.Abs(num) % 2 == 1)
+            {
+                Console.WriteLine("Number: " + num);
+                Console.WriteLine("Result: True");
+            }
+            else
+            {
+                Console.WriteLine("Number: " + num);
+                Console.WriteLine("Result: False");
+            }
+        }
+    }
+}
