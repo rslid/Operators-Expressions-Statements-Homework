bfa9fa0e06010378cc9db6c43a7713d8de92ea98
rslid <takepack@gmail.com>
2014-03-18 20:29:26 +0200

    homework

--- Modify a Bit at Given Position/Bit_at_Given_Position.cs
+++ Modify a Bit at Given Position/Bit_at_Given_Position.cs
@@ -7,17 +7,27 @@ namespace Modify_a_Bit_at_Given_Position
     {
         static void Main()
         {
-            int number,position;
+            int number;
+            int position;
             int value;
+            int mask;
+            int result;
             Console.Write("Please enter number: ");
             number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your number in binary format is " + (Convert.ToString(number, 2).PadLeft(16, '0')));
             Console.Write("Please enter position: ");
             position = int.Parse(Console.ReadLine());
-            Console.Write("Please enter 1 or 0 for value: ");
+            Console.Write("Please enter v (1 or 0): ");
             value = int.Parse(Console.ReadLine());
-            Console.WriteLine("Your number in binary format is "+(Convert.ToString(number, 2).PadLeft(16,'0')));
-            
-
+            mask = value << position;
+            result = number | mask;
+            if (value == 0)
+                {
+                    mask = ~(1 << position);
+                    result = number & mask;
+                }
+            Console.WriteLine("Your new number in binary format is " + (Convert.ToString(result, 2).PadLeft(16, '0')));
+            Console.WriteLine("Your new number is {0}",result);
         }
     }
 }
